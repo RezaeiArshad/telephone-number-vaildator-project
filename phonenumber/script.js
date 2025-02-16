35e51af40e8aacb840f8424faf31a5912d228254
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-02-17 01:55:48 +0330

    Just a little more . . .

--- phonenumber/script.js
+++ phonenumber/script.js
@@ -17,12 +17,43 @@ const clearButton = document.getElementById("clear-btn");
 const checkButton = document.getElementById("check-btn");
 
 const historyData = localStorage.getItem("key") || [];
+let currentNumber = {};
 
-console.log(localStorage.getItem("key"))
+const updateContainer = () => {
+    container.innerHTML = '';
+    historyData.forEach((element => {
+        container.innerHTML += `
+        <div onclick="renderNumber(this)" id="${element.id}" class="history-child">
+          <p id="phone-number">${element.number}</p>
+          <p class="${element.isValid ? "green" : "red"}" id="is-valid">${element.isValid ? "it's valid" : "not valid"}</p>
+        </div>
+        `
+    }))
+ }
+
+ updateContainer()
 
 const updateHistory = () => {
-
     
+    numberObj = {
+       id: `${input.value.split(/^\d/g).join("-")}-${Date.now()}`,
+       number: input.value,
+       isValid: isItValid(), 
+    }
+
+    historyData.unshift(numberObj);
+    localStorage.setItem("key", JSON.stringify(historyData));
+    updateContainer();
+    console.log(localStorage.getItem("key"))
+}
+
+const renderNumber = (callingDiv) => {
+    const arrayIndex = historyData.findIndex(element => element.id === callingDiv.id);
+    
+}
+
+const isItValid = () => {
+    return true;
 }
 
 const checkInput = () => {
@@ -33,6 +64,14 @@ const checkInput = () => {
     isItValid();
     updateHistory();
 }
+
+const dial = (callingButton) => {
+    const buttonNumber = callingButton.id;
+    input.value += buttonNumber
+}
+
+
+
 
 checkButton.addEventListener("click", () => {
     checkInput()
